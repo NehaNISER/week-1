132a569bf5be6752700ebb0f08530cc52f3fb205
saumya-sharma <prasaman.ghosh@niser.ac.in>
2019-08-08 22:16:33 +0530

    solved

---
+++ q1.sh
@@ -0,0 +1,5 @@
+echo "what is your name?"
+mkdir dir1
+cd dir1
+touch a1.cpp
+
---
+++ q2.sh
@@ -0,0 +1,9 @@
+echo "what is your name?"
+mkdir dir2
+cd dir2
+touch a2.cpp
+touch a3.cpp
+touch a4.cpp
+touch a5.cpp
+touch a6.cpp
+
